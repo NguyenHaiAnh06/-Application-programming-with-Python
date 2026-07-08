429f5aaa29488eabd95a69f870ea9759d6849263
NguyenHaiAnh06 <tqx4nb4j7r@privaterelay.appleid.com>
2026-07-08 22:19:44 +0700

    complete commit with OOP

--- luyen_bai.py
+++ luyen_bai.py
@@ -1,10 +1,11 @@
-from abc import ABC , abstractmethod
+from abc import ABC, abstractmethod
+
 
 class Nhanvien(ABC):
-    def __init__(self,mnv,hoten,luongcoban):
+    def __init__(self, mnv, hoten, luongcoban):
         self.manhanvien = mnv
         self.name = hoten
-        self.__luong=luongcoban
+        self.__luong = luongcoban
 
     @abstractmethod
     def tinh_luong(self):
@@ -15,50 +16,66 @@ class Nhanvien(ABC):
 
 
 class luong_van_phong(Nhanvien):
-    def __init__(self,songaycong,mnv,hoten,luongcoban):
-        super().__init__(mnv,hoten,luongcoban)
-        self.songaycong=songaycong
+    def __init__(self, mnv, hoten, luongcoban, songaycong):
+        super().__init__(mnv, hoten, luongcoban)
+        self.songaycong = songaycong
 
     def tinh_luong(self):
         return self.xemluong() + (self.songaycong * 200000)
 
 
 class nhan_vien_ban_hang(Nhanvien):
-    def __init__(self,doanh_so,tl_hoahong,mnv,hoten,luongcoban):
-       super().__init__(mnv,hoten,luongcoban)
-       self.doanhso = doanh_so
-       self.tilehoahong = tl_hoahong
+    def __init__(self, mnv, hoten, luongcoban, doanh_so, tl_hoahong):
+        super().__init__(mnv, hoten, luongcoban)
+        self.doanhso = doanh_so
+        self.tilehoahong = tl_hoahong
 
     def tinh_luong(self):
         return self.xemluong() + (self.doanhso * self.tilehoahong)
 
+
 def main():
     n = int(input())
-    a =[]
+    a = []
     for i in range(n):
         print("Nhap ma nhan vien va ho ten + luong co ban")
         mnv = input()
         hoten = input()
         luongcoban = int(input())
-        print('1 de chon nvvp , 2 de chon nv ban hang')
-        choice = input()
-        if choice == '1':
-            print("Da chon nvvp nhap vao so ngay cong")
-            songaycong = int(input())
-            nvvp = luong_van_phong(songaycong,mnv,hoten,luongcoban)
-            a.append(nvvp)
-        elif choice == '2':
-            print("Da chon nvvp nhap vao doanh so va ti le hoa hong")
-            doanhso = input()
-            tilehoahong = input()
-            nvbn = nhan_vien_ban_hang(doanhso,tilehoahong,mnv,hoten,luongcoban)
-            a.append(nvbn)
-        else:
-            print('lua chon sai mac dinh nvvp so cong = 0')
-            nvvp = luong_van_phong(0,mnv,hoten,luongcoban)
-            a.append(nvvp)
+
+        try:
+            print('1 de chon nvvp , 2 de chon nv ban hang')
+            choice = input()
+
+            if choice == '1':
+                print("Da chon nvvp nhap vao so ngay cong")
+                songaycong = int(input())
+                nv = luong_van_phong(mnv, hoten, luongcoban, songaycong)
+                a.append(nv)
+
+            elif choice == '2':
+                print("Da chon nvbh nhap vao doanh so va ti le hoa hong")
+                doanhso = float(input())
+                tilehoahong = float(input())
+                nv = nhan_vien_ban_hang(mnv, hoten, luongcoban, doanhso, tilehoahong)
+                a.append(nv)
+
+            else:
+                raise ValueError
+
+        except ValueError as e:
+            print(f" lỗi: Khách hàng nhập sai kiểu dữ liệu số hoặc {e}")
+
+        finally:
+            print(f"lượt nhập thứ {i + 1}\n")
 
     for duyetnv in a:
-         print(f'{duyetnv.manhanvien}{duyetnv.name}{duyetnv.xemluong()}{duyetnv.tinh_luong()}')
+        print(f'{duyetnv.manhanvien}\n{duyetnv.name}\n{duyetnv.xemluong()}\n{duyetnv.tinh_luong()}\n')
 
-main()
+    for ktrluong in a:
+        okk = ktrluong.tinh_luong()
+        if 10000000 <= okk <= 20000000:
+            print(f'Thỏa mãn\n{ktrluong.manhanvien}\n{ktrluong.name}\n{okk}\n')
+
+if __name__ == "__main__":
+    main()
